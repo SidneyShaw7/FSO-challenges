fb35913e466bd429a39333825bfa4a65abf2081c
SidneyShaw7 <dmitriev.nikitos@gmail.com>
2023-08-10 21:47:01 -0400

    challenges 2.6-2.7-2.8 completed. some code added

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+function App() {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+
+  const addName = (e) => {
+    e.preventDefault();
+    const nameObject = {
+      name: newName,
+      id: newName,
+      number: newNumber,
+    };
+
+    const personExist = persons.some((person) => person.id === nameObject.id);
+    personExist
+      ? alert(`${nameObject.name} is already added to phonebook`)
+      : setPersons(persons.concat(nameObject)),
+      setNewNumber("");
+    setNewName("");
+  };
+
+  const handleNameChange = (e) => {
+    setNewName(e.target.value);
+  };
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value);
+  };
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => (
+          <li key={person.name}>
+            {person.name} {person.number}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
